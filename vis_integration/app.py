3e9b6576b90165d62533c6f31b2a16d17c37ada4
Kapil Dhanger <dhangerkapil@gmail.com>
2018-10-05 22:50:17 -0400

    Update app.py

--- vis_integration/app.py
+++ vis_integration/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     # Return to the dashboard
- return render_template("static/index.html")
+ return render_template("index.html")
 
 # Route that outputs database results
 @app.route("/getCitiesFromMongo")
